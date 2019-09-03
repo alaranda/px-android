55d30f8cb3e85c36da102fe884c8c52123ddce1f
Gabriel Mileto <gabriel.mileto@mercadolibre.com>
2019-09-03 10:21:14 -0300

    saco variable en desuso

--- src/main/java/com/mercadolibre/constants/Constants.java
+++ src/main/java/com/mercadolibre/constants/Constants.java
@@ -15,7 +15,6 @@ public final class Constants {
     public static final String API_CONTEXT_LOCALHOST = "localhost";
     public static final String API_CONTEXT = "api_context";
     public static final String REQUEST_ID = "request_id";
-    public static final String TEST = "test";
 
     public static final String SERVICE_CONNECTION_TIMEOUT_PROPERTY_KEY = "services.connection.timeout";
     public static final String SERVICE_SOCKET_TIMEOUT_PROPERTY_KEY = "services.socket.timeout";
