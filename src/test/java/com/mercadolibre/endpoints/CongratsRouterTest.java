4afb06c093936a11b5a5aeafd427ed79eb32c926
Gabriel Mileto <gabriel.mileto@mercadolibre.com>
2019-10-01 11:25:33 -0300

    agrego test

--- src/test/java/com/mercadolibre/endpoints/CongratsRouterTest.java
+++ src/test/java/com/mercadolibre/endpoints/CongratsRouterTest.java
@@ -175,4 +175,18 @@ public class CongratsRouterTest {
         final Response response = given().headers(HEADERS).get(uriBuilder.build());
         assertThat(response.getStatusCode(), is(HttpStatus.SC_NOT_FOUND));
     }
+
+    @Test
+    public void getCongrats_invalidHeader_productId() throws URISyntaxException {
+        URIBuilder uriBuilder = new URIBuilder("/px_mobile/congrats")
+                .addParameter(Constants.CALLER_ID_PARAM, USER_ID_TEST)
+                .addParameter(Constants.CLIENT_ID_PARAM, CLIENT_ID_TEST)
+                .addParameter(CALLER_SITE_ID, Site.MLA.getName())
+                .addParameter(PLATFORM, PLATFORM_TEST_MP);
+
+        final Response response = given().headers(new Headers(new Header("accept-language", "es_AR"),
+                new Header("user-agent", "PX/iOS/4.3.4"), new Header(HeadersConstants.DENSITY, DENSITY)))
+                .get(uriBuilder.build());
+        assertThat(response.getStatusCode(), is(HttpStatus.SC_NOT_FOUND));
+    }
 }
