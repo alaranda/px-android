f13f78c4ba8cf688cb8e590fc86e92c3a7cf40c4
Gabriel Mileto <gabriel.mileto@mercadolibre.com>
2019-10-09 15:59:08 -0300

    cambio replace por concat

--- src/main/java/com/mercadolibre/utils/UrlDownloadUtils.java
+++ src/main/java/com/mercadolibre/utils/UrlDownloadUtils.java
@@ -2,9 +2,9 @@ package com.mercadolibre.utils;
 
 public final class UrlDownloadUtils {
 
-    private static final String DOWNLOAD_URL = "https://852u.adj.st/discount_center_payers/list?adjust_t=ufj9wxn&adjust_deeplink=mercadopago%3A%2F%2Fdiscount_center_payers%2Flist&adjust_label=px-PLATFORM";
+    private static final String DOWNLOAD_URL = "https://852u.adj.st/discount_center_payers/list?adjust_t=ufj9wxn&adjust_deeplink=mercadopago%3A%2F%2Fdiscount_center_payers%2Flist&adjust_label=px-";
 
     public static String buildDownloadUrl(final String platform) {
-        return DOWNLOAD_URL.replace("PLATFORM", platform.toLowerCase());
+        return DOWNLOAD_URL.concat(platform.toLowerCase());
     }
 }
