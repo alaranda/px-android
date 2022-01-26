847a12e4e6b1711dd04c5cd69841841641876caa
robarrios <rodolfo.barrios@mercadolibre.com.co>
2022-01-26 16:44:00 -0500

    fix: update track path

--- px-checkout/src/main/java/com/mercadopago/android/px/tracking/internal/events/PostPaymentFlowEvent.kt
+++ px-checkout/src/main/java/com/mercadopago/android/px/tracking/internal/events/PostPaymentFlowEvent.kt
@@ -3,7 +3,7 @@ package com.mercadopago.android.px.tracking.internal.events
 import com.mercadopago.android.px.tracking.internal.TrackFactory
 import com.mercadopago.android.px.tracking.internal.TrackWrapper
 
-private const val EVENT_PATH_POST_PAYMENT_FLOW = "${TrackWrapper.BASE_PATH}/post-payment-flow"
+private const val EVENT_PATH_POST_PAYMENT_FLOW = "${TrackWrapper.BASE_PATH}/post_payment_flow"
 private const val DESTINATION_KEY = "destination"
 
 class PostPaymentFlowEvent @JvmOverloads constructor(private val deepLink: String) : TrackWrapper() {
